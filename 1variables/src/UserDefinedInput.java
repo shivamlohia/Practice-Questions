cab9163564bfe3065220b8089490cd5ab68b3d49
shivamlohia <79108193+shivamlohia@users.noreply.github.com>
2021-03-04 17:47:17 +0530

    Add files via upload

---
+++ 1variables/src/UserDefinedInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//package variables;
+
+public class UserDefinedInput {
+	public static void main(String args[]) {
+		System.out.println("Type anything: ");
+		Scanner x = new Scanner(System.in);
+		System.out.println(x.nextLine());
+		System.out.println("Type anything: ");
+		Scanner y = new Scanner(System.in);
+		System.out.println(y.nextLine());
+		System.out.println("Type anything: ");
+		Scanner z = new Scanner(System.in);
+		System.out.println(z.nextLine());
+	}
+}
---
+++ 1variables/src/variables/Variables.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class var {
+	//import java.util.*;
+	//class new1{
+		public static void main(String args[]){
+			double x=1.4;
+			System.out.print("The value of x is " +x);
+		//}
+	}
+}
